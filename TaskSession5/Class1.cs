eea0711211406771aeac68ebb3026e4b9a532292
mahmoud abd elghfar <mahmoudfazari7@gmail.com>
2026-08-11 03:58:44 +0200

    Add project files.

---
+++ TaskSession5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSession5
+{
+    internal class Book
+    {
+
+        private string password = "secret";
+        internal int copiesInStock = 5;
+        public string Title;
+        public enum generation
+        {
+            Fiction,
+            NonFiction,
+            Science
+        }
+        public Genre BookGenre { get; set; }
+
+    }
+}
---
+++ TaskSession5/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace TaskSession5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task5 Q1
+
+
+
+            ////Compile error , password is accessible only inside its class
+            //Book hmada = new Book();
+            //Console.WriteLine(hmada.password);
+
+            #endregion
+
+            #region Task5 Q2
+
+
+            ////it will compile ,bc internal means the field is accessible from anywhere in the same project
+            //Book hmada = new Book();
+            //Console.WriteLine(hmada.copiesInStock);
+
+            #endregion
+
+            #region Task5 Q3
+
+
+            //Book hmada = new Book();
+            //hmada.Title = "Basics C#";
+            //Console.WriteLine(hmada.Title);
+
+            #endregion
+
+            #region Task5 Q4
+
+            //Book book = new Book();
+            //book.BookGenre = Genre.Science;
+            // Console.WriteLine(book.BookGenre);
+
+
+            #endregion
+
+            #region Task5 Q5
+
+
+            //Console.WriteLine((int)Genre.Fiction);
+            //Console.WriteLine((int)Genre.NonFiction);
+            //Console.WriteLine((int)Genre.Science);
+
+            #endregion
+
+            #region Task5 Q6
+            //int genreNumber = 1;
+            //Genre result = (Genre)genreNumber;
+            //Console.WriteLine(result);
+
+
+            #endregion
+
+            #region Task5 Q7
+
+            //Genre genre = Genre.Fiction;
+            //string result=genre.ToString();
+            //Console.WriteLine(result);
+
+            #endregion
+
+            #region Task5 Q8
+
+            //string genreText = "Science";
+            //Genre result =(Genre)Enum.Parse(typeof(Genre), genreText);
+
+            #endregion
+
+            #region Task5 Q9
+            //string genreText = "Mystery";
+
+            //if (Enum.TryParse<Genre>(genreText, out Genre result))
+            //{
+            //    Console.WriteLine($"{genreText} : is valid genre");
+            //}
+            //else { Console.WriteLine("Unknown genre"); }
+
+
+            #endregion
+
+
+        }
+    }
+}
